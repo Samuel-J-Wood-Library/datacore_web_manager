c4bbb7355e99748bf6da3157e15b91733618eaf9
https://oxpeter@bitbucket.org <oxpeter+git@gmail.com>
2020-01-31 11:33:22 -0500

    fix logic in duplicate user checking

--- models.py
+++ models.py
@@ -380,8 +380,14 @@ class Server(models.Model):
         user_projects = self.get_all_active_users()
         duplicate_user_dict = {}
         for u in user_projects:
-            if (len(user_projects[u]) > 1) & (u.role.name[:9] != 'Data Core'):
-                duplicate_user_dict[u] = user_projects[u]
+            if (len(user_projects[u]) > 1):
+                if u.role:
+                    if u.role.name[:9] == 'Data Core':
+                        continue
+                    else:
+                        duplicate_user_dict[u] = user_projects[u]
+                else:
+                    duplicate_user_dict[u] = user_projects[u]
                 
         return duplicate_user_dict
         
